f77853a1207e915160406430605257d5074a7cb1
rioribeirods <rioribeirods@gmail.com>
2022-09-25 17:49:43 -0300

    :sparkles: feat: first components, assets and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <div>App React</div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-ReactDOM.render(
-  <React.StrictMode>
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 );
