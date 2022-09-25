5347e58607e9ebfa746d0b49beb8ff17da03f20b
rioribeirods <rioribeirods@gmail.com>
2022-09-25 20:16:35 -0300

    :sparkles: login form validation

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import styles from "./Input.module.css"
+import React from 'react';
+import styles from './Input.module.css';
 
-const Input = ({label, type, name, value, onChange }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
-        <label 
-        htmlFor={name} 
-        className={styles.label}>{label}</label>
-        <input 
-        id={name} 
-        name={name} 
-        type={type} 
-        className={styles.input} 
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
         onChange={onChange}
-        value={value} />
-        <p className={styles.error}>Error</p>
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -7,22 +7,24 @@ import useForm from '../../Hooks/useForm';
 const LoginForm = () => {
     const username = useForm();
     const password = useForm();
-    console.log(password.value)
 
     function handleSubmit(event) {
         event.preventDefault();
-        fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(),
-        }).then(response => {
-            console.log(response);
-            return response.json();
-        }).then(json => {
-            console.log(json);
-        });
+        if(username.validate() && password.validate()) {
+            fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(),
+            }).then(response => {
+                console.log(response);
+                return response.json();
+            }).then(json => {
+                console.log(json);
+            });
+        }
+        
     }
 
   return (
