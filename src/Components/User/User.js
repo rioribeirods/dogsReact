6e66ed4cbd522d45042bc7d09e3cba979c5fabcf
rioribeirods <rioribeirods@gmail.com>
2022-09-26 14:12:50 -0300

    :sparkles: mobile menu at /conta

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import UserHeader from './UserHeader'
+import Feed from "../Feed/Feed"
+import UserPhotoPost from "./UserPhotoPost"
+import UserStats from './UserStats'
 
 const User = () => {
   return (
-    <div>Usuário</div>
+    <section className='container'>
+      <UserHeader />
+      <Routes>
+        <Route path='/' element={<Feed />} />
+        <Route path='posta' element={<UserPhotoPost />} />
+        <Route path='estatisticas' element={<UserStats />} />
+      </Routes>
+    </section>
   )
 }
 
