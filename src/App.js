d7c0e1fbbad984d967c6dfbd8ce4f43e288d890e
rioribeirods <rioribeirods@gmail.com>
2022-09-25 21:02:07 -0300

    :sparkles: useContext for users

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
 
     </>
