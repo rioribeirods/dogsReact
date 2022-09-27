3b97240d3186a00e7306a768ca965d028aa69859
rioribeirods <rioribeirods@gmail.com>
2022-09-26 21:55:34 -0300

    :bug: fix: minha conta aparecendo o feed padrão

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import UserHeader from './UserHeader'
-import Feed from "../Feed/Feed"
-import UserPhotoPost from "./UserPhotoPost"
-import UserStats from './UserStats'
+import React from 'react';
+import UserHeader from './UserHeader';
+import { Routes, Route } from 'react-router-dom';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+import { UserContext } from '../../UserContext';
+import NotFound from '../NotFound';
+import Head from '../Helper/Head';
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
-    <section className='container'>
+    <section className="container">
+      <Head title="Minha Conta" />
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />} />
-        <Route path='postar' element={<UserPhotoPost />} />
-        <Route path='estatisticas' element={<UserStats />} />
+        <Route path="/" element={<Feed user={data.id} />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
-  )
-}
+  );
+};
 
-export default User
+export default User;
